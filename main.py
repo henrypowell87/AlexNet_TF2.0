0253ef243596b63a25f49168b5cf8be8cb3477ca
Henry Powell <henryp@henryp-mac.psy.gla.ac.uk>
2019-10-16 16:32:17 +0100

    Softmax output visualization removed

--- main.py
+++ main.py
@@ -169,41 +169,6 @@ def run_training(train_images, train_labels, test_images, test_labels, val_image
 
     return model
 
-
-def plot_image(i, predictions_array, true_label, img, info):
-    class_names = info.features['label'].names
-    predictions_array, true_label, img = predictions_array, true_label[i], img[i]
-    plt.grid(False)
-    plt.xticks([])
-    plt.yticks([])
-
-    plt.imshow(img)
-
-    predicted_label = np.argmax(predictions_array)
-    if predicted_label == true_label:
-        color = 'blue'
-    else:
-        color = 'red'
-
-    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-                                100*np.max(predictions_array),
-                                class_names[true_label]),
-                                color=color)
-
-
-def plot_value_array(i, predictions_array, true_label):
-    predictions_array, true_label = predictions_array, true_label[i]
-    plt.grid(False)
-    plt.xticks(range(10))
-    plt.yticks([])
-    thisplot = plt.bar(range(10), predictions_array, color="#777777")
-    plt.ylim([0, 1])
-    predicted_label = np.argmax(predictions_array)
-
-    thisplot[predicted_label].set_color('red')
-    thisplot[true_label].set_color('blue')
-
-
 def predictions(model, test_images, test_labels):
 
     predictions = model.predict(test_images)
@@ -221,15 +186,6 @@ def run_experiment(n):
         plt.show()
         model = run_training(train_images, train_labels, test_images, test_labels, val_images, val_labels)
         predictions(model, test_images, test_labels)
-        num_rows = 5
-        num_cols = 3
-        num_images = num_rows * num_cols
-        plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
-        for i in range(num_images):
-            plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
-            plot_image(i, predictions[i], test_labels, test_images, info)
-            plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
-            plot_value_array(i, predictions[i], test_labels)
         plt.tight_layout()
         plt.show()
 
